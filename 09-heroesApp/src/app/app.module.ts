e9c9d9cc36ba44f27809594934cc429463fe2088
Cristian Rafael De Leon Ronquillo <crdeleon@ric.local>
2020-03-16 16:33:08 -0600

    crud

---
+++ 09-heroesApp/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'heroe/:id', component: HeroeComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'heroes' }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroeComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot( routes ),
+    FormsModule
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 09-heroesApp/src/app/pages/heroe/heroe.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from '../../models/heroe.model';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['./heroe.component.css']
+})
+export class HeroeComponent implements OnInit {
+
+  heroe = new HeroeModel();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  guardar( form: NgForm ) {
+
+    if (form.invalid) {
+      return;
+    }
+
+    console.log( form );
+  }
+}
