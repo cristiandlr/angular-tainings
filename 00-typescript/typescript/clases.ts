53dfc694255daba3068a01479ff4298b3367fec6
cristiandlr <cristiandlr@gmail.com>
2020-06-18 16:22:45 -0600

    firebase, last lesson

--- 00-typescript/typescript/clases.ts
+++ 00-typescript/typescript/clases.ts
@@ -1,30 +1,44 @@
-"use strict";
-(() => {
+(()=>{
+
+
     // class Avenger {
+
     //     nombre: string;
     //     equipo: string;
     //     nombreReal: string;
+
     //     puedePelear: boolean;
     //     peleasGanadas: number;
+
     //     constructor( nombre: string, equipo: string ){
     //         this.nombre = nombre;
     //         this.equipo = equipo;
     //     }
+        
     // }
+
     class Avenger {
+
         // nombre: string;
         // equipo: string;
         // nombreReal: string;
+
         // puedePelear: boolean;
         // peleasGanadas: number;
-        constructor(nombre, equipo, nombreReal, puedePelear = true, peleasGanadas = 0) {
-            this.nombre = nombre;
-            this.equipo = equipo;
-            this.nombreReal = nombreReal;
-            this.puedePelear = puedePelear;
-            this.peleasGanadas = peleasGanadas;
-        }
+
+        constructor( public nombre: string,
+                     public equipo: string,
+                     public nombreReal?: string,
+                     public puedePelear: boolean  = true,
+                     public peleasGanadas: number = 0){}
+        
     }
+
     const antman = new Avenger('Antman', 'Capi');
+    
     console.log(antman);
+
 })();
+
+
+
